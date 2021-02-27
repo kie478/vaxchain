3a19a22b54df8fcac1451c645de4daeb3afeee66
kie478 <78882929+kie478@users.noreply.github.com>
2021-02-27 00:33:56 -0500

    Add files via upload

--- Block.java
+++ Block.java
@@ -5,15 +5,25 @@ import java.text.SimpleDateFormat;
 
 
 public class Block {
-	private String hashCode, timeStamp;
-	 
-	public Block(VacinationData vaxData) {
+	public String hash, previousHash, timeStamp;
+	
+	public Block(VacinationData vaxData, String previousHash) {
 		this.timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss.ms").format(new Date());
-		this.hashCode = MD5.getMd5(timeStamp + vaxData.getFirstName() + vaxData.getLastName());
+		this.hash = MD5.getMd5(timeStamp + vaxData.getFirstName() + vaxData.getLastName() + String.valueOf(vaxData.getZipCode()));		
+	
 	}
 
-	public String getHashCode() {
-		return hashCode;
+	public String getHash() {
+		return hash;
+	}
+	
+	public void setAsPreviousHash(String hash) {
+		this.previousHash = hash;
+	}	
+	
+	public String getPreviousHash() {
+		return previousHash;
 	}
 
+	
 }
