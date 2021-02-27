1e7552692bbd54cae3482d1b4da0e91eb8910d52
Gresba <79733876+Gresba@users.noreply.github.com>
2021-02-27 10:58:54 -0500

    Update and rename Block to Block.java

---
+++ Block.java
@@ -0,0 +1,42 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Block{
+    private String hash, previousHash, timeStamp;
+    private VacinationData data;
+
+    public Block(VacinationData data, String previousHash){
+        this.data = data;
+        this.previousHash = "";
+        this.timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+        this.hash = calculateHash();
+    }
+
+    private String calculateHash()
+    {
+        String preHash = data.getFirstName() + data.getLastName() + data.getAge() + timeStamp;
+        hash = MD5.getMd5(preHash);
+        return hash;
+    }
+
+
+    public String getPreviousHash()
+    {
+        return previousHash;
+    }
+
+    public void setPreviousHash(String previousHash)
+    {
+        this.previousHash = previousHash;
+    }
+
+    public String getHash()
+    {
+        return hash;
+    }
+
+    public void setHash(String hash)
+    {
+        this.hash = hash;
+    }
+}
