7dbf31ca56f3e3a7ea7f7b12842683877d1e95df
Gresba <79733876+Gresba@users.noreply.github.com>
2021-02-28 01:03:01 -0500

    Update Block.java

--- Block.java
+++ Block.java
@@ -1,24 +1,34 @@
+package BlockChainData;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Block{
     private String hash, previousHash, timeStamp;
     private VaccinationData data;
+    private static int counter;
+    private int iD;
 
     public Block(VaccinationData data, String previousHash){
         this.data = data;
-        this.previousHash = "";
+        this.previousHash = previousHash;
         this.timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
         this.hash = calculateHash();
+        this.iD = counter++;
     }
 
-    private String calculateHash()
+    public VaccinationData getVaccData()
+    {
+        return this.data;
+    }
+
+
+    public String calculateHash()
     {
         String preHash = data.getFirstName() + data.getLastName() + data.getAge() + timeStamp;
         hash = MD5.getMd5(preHash);
         return hash;
     }
-
 
     public String getPreviousHash()
     {
@@ -38,5 +48,9 @@ public class Block{
     public void setHash(String hash)
     {
         this.hash = hash;
+    }
+
+    public int getiD() {
+        return iD;
     }
 }
