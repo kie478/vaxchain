33e3def090ad92413fae29ac3fbbd2a4c7cf3aaf
Gresba <79733876+Gresba@users.noreply.github.com>
2021-02-28 01:02:47 -0500

    Update BlockChain.java

--- BlockChain.java
+++ BlockChain.java
@@ -1,8 +1,10 @@
+package BlockChainData;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class BlockChain{
-    private List<Block> chain;
+    public List<Block> chain;
     public BlockChain(){
         chain = new ArrayList<Block>();
         chain.add(createGenesisBlock());
@@ -10,7 +12,7 @@ public class BlockChain{
 
     private Block createGenesisBlock()
     {
-        return new Block(new VaccinationData("FIRST_NAME", "LAST_NAME", 0), "0");
+        return new Block(new VaccinationData("GENESIS", "BLOCK", 0), "NO_HASH");
     }
 
     public void addBlock(Block block)
@@ -22,5 +24,30 @@ public class BlockChain{
     public Block getPreviousBlock()
     {
         return chain.get(chain.size() - 1);
+    }
+
+    public boolean isHashValid(int i)
+    {
+        if(!(chain.get(i).getPreviousHash().equals(chain.get(i - 1).getHash())))
+            return false;
+        return true;
+    }
+
+    public Block getBlock(int i)
+    {
+        return chain.get(i);
+    }
+
+    public boolean isChainValid(){
+        for(int i = 1; i < chain.size(); i++){
+            Block currentBlock = chain.get(i);
+            Block previousBlock = chain.get(i - 1);
+
+            if(!currentBlock.getPreviousHash().equals(previousBlock.getHash()))
+                return false;
+            if(!currentBlock.getHash().equals(currentBlock.calculateHash()))
+                return false;
+        }
+        return true;
     }
 }
